4619cc8c9b48be22d2de455ab6a880c8d3ef7d55
Md. Maruf Suborno <64451255+suborno180@users.noreply.github.com>
2023-03-16 16:18:09 +0600

    update

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -16,7 +16,7 @@ const About = () => {
           animate={{ x: 100 }}
           initial={{ x: 0 }}
           transition={{ duration: 1 }
-          }>{ }
+          }>
           This is my animated element.
         </motion.div>
         <div className='container mx-96'>
