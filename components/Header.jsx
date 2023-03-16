b1d24135358c9fc9f4d1a027f7ca1b9f87ceff9e
Md. Maruf Suborno <64451255+suborno180@users.noreply.github.com>
2023-03-16 16:11:47 +0600

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import Link from 'next/link'
 import MobileNav from './MobileNav'
+import { motion } from 'framer-motion'
 
 const Header = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+  const List = ['home', 'about', 'contact', 'survices']
   return (
     <>
       <header className='bg-pink-200 flex fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] items-center'>
         <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between' >
           <Link href={'/'} className="text-[25px] font-semibold" >Logo</Link>
-          <nav className='hidden lg:flex'>
-              <Link className='hover:text-black px-5 text-[#222222] hover:text-primary transition' href={'/'}>Home</Link>
-              <Link className='hover:text-black px-5 text-[#696c6d] hover:text-primary transition' href={'/about'}>About</Link>
-              <Link className='hover:text-black px-5 text-[#696c6d] hover:text-primary transition' href={'/contsct'}>contact</Link>
+          <nav>
+            <motion.ul
+              variants={container}
+              initial="hidden"
+              animate="visible"
+              className='hidden lg:flex uppercase'>
+              {List.map(e => <motion.li key={e} variants={item} className='px-3 hover:text-black'><Link href={'/'} >{e}</Link></motion.li>)}
+            </motion.ul>
           </nav>
         </div>
-          <MobileNav/>
+        <MobileNav />
       </header>
     </>
   )
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdClose } from 'react-icons/io'
 import { CgMenuRight } from 'react-icons/cg'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const MobileNav = () => {
+
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+          }
+        }
+      };
+      const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1
+        }
+      };
+      const List = ['home', 'about', 'contact', 'survices']
+      const [openMenu, setOpenMenu] = useState(false)
     return (
         <>
             <nav className='lg:hidden'>
-                <button className="text-3xl cursor-pointer" ><CgMenuRight /></button>
-                <div className='bg-white shadow-2xl absolute top-0 right-0 w-full max-w-xs h-screen' >
-                    <button className='text-3xl mx-4 my-5'><IoMdClose /></button>
-                    <ul className='flex flex-col justify-center items-center h-screen uppercase text-2xl'>
-                        <li className='py-3'><Link href={'/'} >home</Link></li>
-                        <li className='py-3'><Link href={'/about'} >about</Link></li>
-                        <li className='py-3'><Link href={'/contact'} >contact</Link></li>
-                        <li className='py-3'><Link href={'/survices'} >Survices</Link></li>
-                    </ul>
-                </div>
+                <button className="text-3xl cursor-pointer" onClick={()=>setOpenMenu(true)} ><CgMenuRight /></button>
+                <motion.div 
+                variants={container}
+                initial={{x: "100%"}}
+                animate={openMenu ? {x: 0} : ""}
+                className='bg-white shadow-2xl absolute top-0 right-0 w-full max-w-xs h-screen' >
+                    <button className='text-3xl mx-4 my-5' onClick={()=>setOpenMenu(false)}><IoMdClose /></button>
+                    <motion.ul 
+                    variants={container}
+                    initial="hidden"
+                    animate={openMenu ? "visible" : ""}
+                    className='flex flex-col justify-center items-center h-screen uppercase text-2xl font-bold text-[#5f5f5f]'>
+                        {List.map(e=><motion.li key={e} variants={item} className='py-3 hover:text-black'><Link href={'/'} >{e}</Link></motion.li>)}
+                    </motion.ul>
+                </motion.div>
             </nav>
         </>
     )
