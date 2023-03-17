9c7703523b8ecf3ed5aff523c7908e456cbcb7b9
Md. Maruf Suborno <64451255+suborno180@users.noreply.github.com>
2023-03-17 22:03:52 +0600

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   const writeData = async () => {
     addDoc(collection(firebase, 'strafs'), mainData)
   }
-  const num = [0,1,2,3,4]
+  const num = [0, 1, 2, 3, 4]
   return (
     <>
       <Head>
@@ -56,16 +56,19 @@ export default function Home() {
         <div className="container mx-auto">
           <div className="flex flex-col justify-center">
             <motion.div
-            variants={container}
-            initial="hidden"
-            animate="visible"
-              className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start lg:top-[35%]">
-              <motion.h1 variants={item} className='text-[35px] block lg:text-[50px]'>
+              variants={container}
+              initial="hidden"
+              animate="visible"
+              className="w-full pt-36 pb-14 lg:py-[1000px] lg:pb-0 lg:w-auto z-10 flex lg:absolute flex-col justify-center items-center lg:items-start">
+              <motion.h1 variants={item} className='text-[45px] block lg:text-[100px] font-bold'>
                 Web Designer <br /> & Developer
               </motion.h1>
-              <motion.p variants={item}  className='text-[20px] my-2 lg:text-[20px]'>Rajshahi, Bangladesh</motion.p>
-              <motion.span variants={item} className="py-5" ><Link href={'/'} className='py-2 px-4 bg-black text-white my-4 hover:bg-transparent hover:text-black hover:border-x-2 hover:border-black transition' >Hire me</Link></motion.span>
+              <motion.p variants={item} className='text-[26px] my-0 lg:text-[36px]'>Rajshahi, Bangladesh</motion.p>
+              <motion.span variants={item} className="py-5 lg:my-4" ><Link href={'/'} className='py-3 px-7 bg-black text-white my-4 hover:bg-transparent hover:text-black hover:border-x-2 hover:border-black transition' >Hire me</Link></motion.span>
             </motion.div>
+          </div>
+          <div className='flex justify-end max-h-96 lg:max-h-max'>
+            <div className='relative lg:-right-40 bg-blue-500 overflow-hidden'></div>
           </div>
         </div>
       </div>
--- src/pages/survices/index.jsx
+++ src/pages/survices/index.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import Head from 'next/head'
 import { motion } from 'framer-motion'
 import Header from '../../../components/Header'
+import Link from 'next/link'
 
 const Survices = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
+
+  const num = [0, 1, 2, 3, 4]
   return (
     <>
       <Head>
@@ -14,11 +36,24 @@ const Survices = () => {
       </Head>
       <Header />
       {/* Slider Part */}
-      <div className="slider h-96 bg-purple-600">
-       
-        <div className='container mx-10 pt-[100px]'>
-          <h1 className='font-medium' >Protfolio</h1>
-        <motion.h1 initial={{x: "100%"}} animate={{x: 0}} transition={{duration: 1}}>Survices Page</motion.h1>
+      <div className="section">
+        <div className="container mx-auto">
+          <div className="flex flex-col justify-center">
+            <motion.div
+              variants={container}
+              initial="hidden"
+              animate="visible"
+              className="w-full pt-36 pb-14 lg:py-[1000px] lg:pb-0 lg:w-auto z-10 flex lg:absolute flex-col justify-center items-center lg:items-start">
+              <motion.h1 variants={item} className='text-[45px] block lg:text-[100px] font-bold'>
+                Our Survices
+              </motion.h1>
+              <motion.p variants={item} className='text-[26px] my-0 lg:text-[36px]'>Rajshahi, Bangladesh</motion.p>
+              <motion.span variants={item} className="py-5 lg:my-4" ><Link href={'/'} className='py-3 px-7 bg-black text-white my-4 hover:bg-transparent hover:text-black hover:border-x-2 hover:border-black transition' >Hire me</Link></motion.span>
+            </motion.div>
+          </div>
+          <div className='flex justify-end max-h-96 lg:max-h-max'>
+            <div className='relative lg:-right-40 bg-blue-500 overflow-hidden'></div>
+          </div>
         </div>
       </div>
     </>
