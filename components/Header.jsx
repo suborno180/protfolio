4f5576212f56577433502a85b8fe088794faa449
suborno180 <64451255+suborno180@users.noreply.github.com>
2023-07-17 17:42:15 +0600

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Link from 'next/link'
-import MobileNav from './MobileNav'
-import SocileMediaLinks from '../components/SocileMediaLinks'
-import { motion } from 'framer-motion'
+import React from "react";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+import SocileMediaLinks from "../components/SocileMediaLinks";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const container = {
@@ -12,58 +12,71 @@ const Header = () => {
       scale: 1,
       transition: {
         delayChildren: 0.3,
-        staggerChildren: 0.2
-      }
-    }
+        staggerChildren: 0.2,
+      },
+    },
   };
 
   const item = {
     hidden: { y: 20, opacity: 0 },
     visible: {
       y: 0,
-      opacity: 1
-    }
+      opacity: 1,
+    },
   };
   const List = [
     {
       name: "home",
-      loc: "/"
+      loc: "/",
     },
     {
       name: "about",
-      loc: "/about"
-    },
-    {
-      name: "contact",
-      loc: "/contact"
+      loc: "/about",
     },
     {
       name: "survices",
-      loc: "/survices"
+      loc: "/survices",
     },
-  ]
+  ];
   return (
     <>
-      <header className='flex fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] items-center'>
-        <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between' >
-          <Link href={'/'} className="text-[25px] font-semibold flex items-center" >
-            <img src="/logo.svg" alt="logo" className='h-10 mr-1' />
-          </Link>
-          <nav>
-            <ul
-              variants={container}
-              initial="hidden"
-              animate="visible"
-              className='hidden lg:flex uppercase'>
-              {List.map(e => <li key={e.id} variants={item} className='px-3 hover:text-black'><Link href={`/${e.loc}`} >{e.name}</Link></li>)}
-            </ul>
-          </nav>
+      <header className="flex fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] items-center">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+            <Link
+              href={"/"}
+              className="text-[25px] font-semibold flex items-center"
+            >
+              <img src="/logo.svg" alt="logo" className="h-10 mr-1" />
+            </Link>
+            <nav>
+              <ul
+                variants={container}
+                initial="hidden"
+                animate="visible"
+                className="hidden lg:flex uppercase items-center"
+              >
+                {List.map((e) => (
+                  <li
+                    key={e.id}
+                    variants={item}
+                    className="px-3 hover:text-black"
+                  >
+                    <Link href={`/${e.loc}`}>{e.name}</Link>
+                  </li>
+                ))}
+                <li className="btn bg-gradient-to-r from-amber-500 to-amber-400 shadow-lg shadow-yellow-500/80 hover:scale-105 transition-all cursor-pointer p-2 px-6 rounded-full text-white ml-14" >
+                  <Link href={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <SocileMediaLinks />
+        {/* <SocileMediaLinks /> */}
         <MobileNav />
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Header from '../../components/Header'
-import { Inter } from 'next/font/google'
-import { motion } from 'framer-motion';
-import { getFirestore, addDoc, collection } from 'firebase/firestore'
-import { app } from '../../components/firebase'
-import { useState } from 'react';
-import Iframe from 'react-iframe';
-import { HiArrowRight } from "react-icons/hi";
-import Clients from '../../components/Clients '
-import Link from 'next/link';
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { motion } from "framer-motion";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Header from "../../components/Header";
+import { app } from "../../components/firebase";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const container = {
@@ -20,30 +18,29 @@ export default function Home() {
       scale: 1,
       transition: {
         delayChildren: 0.3,
-        staggerChildren: 0.2
-      }
-    }
+        staggerChildren: 0.2,
+      },
+    },
   };
   const item = {
     hidden: { y: 20, opacity: 0 },
     visible: {
       y: 0,
-      opacity: 1
-    }
+      opacity: 1,
+    },
   };
 
-
-  const firebase = getFirestore(app)
+  const firebase = getFirestore(app);
   const [mainData, setMainData] = useState({
     irst_name: "maruf",
     last_name: "Subonro",
-    age: 19
-  })
+    age: 19,
+  });
 
   const writeData = async () => {
-    addDoc(collection(firebase, 'strafs'), mainData)
-  }
-  const num = [0, 1, 2, 3, 4]
+    addDoc(collection(firebase, "strafs"), mainData);
+  };
+  const num = [0, 1, 2, 3, 4];
   return (
     <>
       <Head>
@@ -54,37 +51,90 @@ export default function Home() {
       <Header />
       {/* Slider Part */}
       <div className="section">
-        <div className="container mx-auto">
-          <motion.div variants={container} initial="hidden" animate="visible" className='lg:flex lg:justify-between'>
-            <div className='pt-32  flex flex-col justify-center items-center lg:items-start lg:w-[60%]'>
-              <motion.h1 variants={item} className='text-[45px] font-bold lg:text-[80px]'>Web Designer <br /> & Developer</motion.h1>
-              <motion.p variants={item}  className='text-[25px] my-5'>Rajshahi, Bangladesh</motion.p>
-              {/* <motion.span variants={item} className="py-5 lg:my-4" ><Link href={'/'} className='py-3 px-7 bg-black text-white my-4 hover:bg-transparent hover:text-black hover:border-x-2 hover:border-black transition' >Hire me</Link></motion.span> */}
-          <motion.span variants={item} className="py-5 lg:my-4 flex items-center text-[18px] font-bold" ><HiArrowRight /><Link href={'/'} className="ml-2" >Hire me</Link> </motion.span>
+        <div className="relative  w-ful h-[100vh]">
+          <div className="absolute top-[25%] -left-32 rounded-full  w-[450px] h-[450px] bg-[#c8a1fa]"></div>
+          <div className="absolute top-[15%] right-32 rounded-full  w-[350px] h-[350px] bg-[#95e9f6]"></div>
+          <div className="absolute bottom-[15%] left-[40rem] rounded-full  w-[450px] h-[450px] bg-[#bfd9f6]"></div>
+          <div className="backdrop-blur-xl bg-white/80 bsolute top-0 left-0 w-full h-full">
+            {/* main content */}
+            <div className="container mx-auto">
+              <div className="grid md:grid-cols-2 h-[100vh]">
+                <div className="px-5 md:px-0 w-full md:h-full flex flex-col items-start justify-center">
+                  <motion.h1
+                    initial={{ translateX: -250 }}
+                    transition={{ duration: 0.5 }}
+                    whileInView={{ translateX: 0 }}
+                    className="text-6xl md:text-7xl font-bold"
+                  >
+                    Hy! Am <br />{" "}
+                    <span className="text-orange-500">Maruf Subonro</span>
+                  </motion.h1>
+                  <motion.p
+                    initial={{ translateX: -250 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                    whileInView={{ translateX: 0 }}
+                    className="w-[80%] my-5"
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vel labore quia incidunt! Molestiae nam tempora maiores.
+                    Distinctio earum iusto aspernatur!
+                  </motion.p>
+                  <motion.div
+                    initial={{ scale: 0 }}
+                    transition={{ duration: 0.5, delay: 0.4 }}
+                    whileInView={{ scale: 1 }}
+                  >
+                    <Link
+                      href={"/contact"}
+                      className="btn bg-gradient-to-r from-amber-500 to-amber-400 shadow-lg shadow-yellow-500/80 hover:scale-105 transition-all cursor-pointer p-2 px-6 rounded-full text-white"
+                    >
+                      Hire me
+                    </Link>
+                  </motion.div>
+                </div>
+                <div className="relative px-5 md:px-0 w-full md:h-full flex flex-col items-start justify-center">
+                  <motion.div
+                  initial={{ scale: 0.8 }}
+                  transition={{ duration: 0.5, delay: 0.4 }}
+                  whileInView={{ scale: 1 }}
+                  >
+                  <Image src="/hero-image.png" alt="hero image" width={700} height={700} />
+                  </motion.div>
+                  <div className="absolute top-[20%] right-0">
+                  <div className="card flex items-center bg-white shadow-xl scale-75 hover:scale-100 transition-all cursor-pointer p-5 rounded-lg">
+                    <div className="w-10 h-10 grid place-content-center rounded-full bg-pink-200">UI</div>
+                    <div className="flex flex-col items-start ml-5">
+                      <strong>UI/UX</strong>
+                      <p className="w-[250px]">Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='w-[100%] lg:h-screen mt-10 lg:my-0 flex justify-center lg:justify-end'>
-              <div className='w-[100%] h-96 lg:h-screen bg-purple-500'></div>
-            </div>
-          </motion.div>
-          
+          </div>
         </div>
       </div>
       <div className="section">
-        <div className='h-96 bg-gray-900 lg:py-8'>
-          <div className="container m-auto">
-          <div className='p-5'>
-            <span className='text-6xl font-bold text-gray-700 lg:text-8xl'>About</span>
-            <h2 className='text-3xl -mt-6 lg:-mt-10 text-gray-50 lg:text-5xl'>About</h2>
-          </div>
-          <div className='text-3xl p-4 text-gray-500 lg:text-5xl'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, laborum. Quae quam sunt eaque voluptatum!</p>
-          </div>
+        <div className="relative  w-ful h-[100vh]">
+          <div className="absolute top-[25%] -left-32 rounded-full  w-[450px] h-[450px] bg-[#faa1df]"></div>
+          <div className="absolute top-[15%] right-32 rounded-full  w-[350px] h-[350px] bg-[#ba95f6]"></div>
+          <div className="absolute bottom-[15%] left-[40rem] rounded-full  w-[450px] h-[450px] bg-[#f6ddbf]"></div>
+          <div className="backdrop-blur-xl bg-white/80 bsolute top-0 left-0 w-full h-full">
+            {/* main content */}
           </div>
         </div>
       </div>
       <div className="section">
-        <Clients />
+        <div className="relative  w-ful h-[100vh]">
+          <div className="absolute top-[25%] -left-32 rounded-full  w-[450px] h-[450px] bg-[#a1ebfa]"></div>
+          <div className="absolute top-[15%] right-32 rounded-full  w-[350px] h-[350px] bg-[#95f6b1]"></div>
+          <div className="absolute bottom-[15%] left-[40rem] rounded-full  w-[450px] h-[450px] bg-[#f6c8bf]"></div>
+          <div className="backdrop-blur-xl bg-white/80 bsolute top-0 left-0 w-full h-full">
+            {/* main content */}
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
